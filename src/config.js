67bac5016c5846b51e300282ee310295da550b5d
Tom Moitié <tom@tmoitie.co.uk>
2015-12-15 02:27:09 +0000

    Update z-index of modal to be higher and reduce season end

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
 export const seasonStart = parseInt(moment('2015-12-15 12:00:00').format('X'), 10);
-export const seasonEnd = parseInt(moment('2016-03-08 12:00:00').format('X'), 10);
+export const seasonEnd = parseInt(moment('2016-03-07 12:00:00').format('X'), 10);
