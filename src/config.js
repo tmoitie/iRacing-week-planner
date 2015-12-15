453b1ef3975346a65ccb3de750e0f42f77588917
Tom Moitié <tom@tmoitie.co.uk>
2015-12-15 00:08:54 +0000

    add new season

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
-export const seasonStart = parseInt(moment('2015-12-08 12:00:00').format('X'), 10);
-export const seasonEnd = parseInt(moment('2015-12-14 12:00:00').format('X'), 10);
+export const seasonStart = parseInt(moment('2015-12-15 12:00:00').format('X'), 10);
+export const seasonEnd = parseInt(moment('2016-03-08 12:00:00').format('X'), 10);
