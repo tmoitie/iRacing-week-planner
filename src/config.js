44c664160f7f6caea9a9a6d640bcd766d0e2b744
Tom Moitié <tom@tmoitie.co.uk>
2016-02-22 02:49:25 +0000

    Update season dates to go to the end of season 2.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
-export const seasonStart = moment('2015-12-15 00:00:00').utc();
-export const seasonEnd = moment('2016-03-07 00:00:00').utc();
+export const seasonStart = moment('2016-02-09 00:00:00').utc();
+export const seasonEnd = moment('2016-06-14 00:00:00').utc();
