3c24388c40f6dba22516663c49ddd2e0723d8188
Tom Moitié <tomm@magmadigital.co.uk>
2016-12-15 14:56:58 +0000

    Change season End variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
 export const seasonStart = moment('2016-12-13T00:00:00+00:00').utc();
-export const seasonEnd = moment('2017-03-27T00:00:00+00:00').utc();
+export const seasonEnd = moment('2017-03-13T00:00:00+00:00').utc();
 export const weekSeasonStart = moment('2016-12-13T00:00:00+00:00').utc();
