35b6f3ef23a946ca46a47424c9a2493a11f1f645
Tom Moitié <tom@tmoitie.co.uk>
2017-02-08 00:12:16 +0000

    Add a scraper to automate data import

---
+++ scrape.js
@@ -0,0 +1,31 @@
+import { remote } from 'webdriverio';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'bluebird';
+
+const writeFile = promisify(fs.writeFile);
+
+const client = remote({
+  logLevel: 'command',
+  desiredCapabilities: { browserName: 'phantomjs' }
+}).init();
+
+const username = process.env.IWP_USERNAME || 'test';
+const password = process.env.IWP_PASSWORD || 'test';
+
+client
+  .url('https://members.iracing.com/membersite/login.jsp')
+  .addValue('[name="username"]', username)
+  .addValue('[name="password"]', password)
+  .click('input.log-in')
+  .url('http://members.iracing.com/membersite/member/Series.do')
+  .waitForVisible('.simpleNav #navigation', 5000)
+  .execute('return TrackListing;')
+  .then(result => writeFile(path.join(__dirname, 'src/data/tracks.json'), JSON.stringify(result.value, null, 2)))
+  .execute('return CarClassListing;')
+  .then(result => writeFile(path.join(__dirname, 'src/data/car-class.json'), JSON.stringify(result.value, null, 2)))
+  .execute('return CarListing;')
+  .then(result => writeFile(path.join(__dirname, 'src/data/cars.json'), JSON.stringify(result.value, null, 2)))
+  .execute('return SeasonListing;')
+  .then(result => writeFile(path.join(__dirname, 'src/data/season.json'), JSON.stringify(result.value, null, 2)))
+  .end();
