2df5ce64a185e15ea39ff3fe55fffb9e4dbfff35
Tom Moitié <tom@tmoitie.co.uk>
2017-04-14 16:44:23 +0100

    Update Blancpain offweeks

--- src/data/raceTimes.js
+++ src/data/raceTimes.js
@@ -394,7 +394,7 @@ export default [
     ],
     weekStartOffset: duration({ days: 2 }),
     weekEndOffset: duration({ days: 7 }),
-    offWeeks: [1.1, 1.2, 1.3, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 3.1, 3.2, 3.3, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 5.1, 5.2]
+    offWeeks: [1.5, 3.1, 3.2, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6]
   }, {
     seriesId: 280,
     setTimes: [
