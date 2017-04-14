f0ae6cecb7300448117a9bdf0b5faac2c8b2bcb1
Tom Moitié <tom@tmoitie.co.uk>
2017-04-14 16:44:12 +0100

    Add debug series script

---
+++ debugSeries.js
@@ -0,0 +1,33 @@
+import Table from 'cli-table';
+import moment from 'moment';
+
+import allRaces from './src/lib/races';
+
+const seriesId = parseInt(process.argv[2], 10);
+const races = allRaces.filter(race => race.seriesId === seriesId);
+
+const table = new Table({
+  head: [
+    'ID', 'Name', 'Week Length', 'Start', 'End', 'Season Start', 'Season End'
+  ],
+  chars: {
+    top: '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗',
+    bottom: '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝',
+    left: '║', 'left-mid': '╟',
+    mid: '─', 'mid-mid': '┼',
+    right: '║', 'right-mid': '╢',
+    middle: '│'
+  }
+});
+
+table.push(...races.map(race => [
+  race.seriesId,
+  race.series,
+  race.weekLength.asDays(),
+  moment(race.startTime).local().format('ddd YYYY-MM-DD'),
+  moment(race.endTime).local().format('ddd YYYY-MM-DD'),
+  moment(race.seriesStart).local().format('ddd YYYY-MM-DD'),
+  moment(race.seriesEnd).local().format('ddd YYYY-MM-DD'),
+]));
+
+console.log(table.toString());
