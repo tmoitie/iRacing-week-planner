7625ff1eee46060902d4b9e70ba208996095c713
Tom Moitié <tom@tmoitie.co.uk>
2017-06-11 14:56:11 +0100

    switch to source reading

--- scrape.js
+++ scrape.js
@@ -7,11 +7,26 @@ const writeFile = promisify(fs.writeFile);
 
 const client = remote({
   logLevel: 'command',
-  desiredCapabilities: { browserName: 'chrome' }
+  desiredCapabilities: { browserName: 'phantomjs' }
 }).init();
 
 const username = process.env.IWP_USERNAME || 'test';
 const password = process.env.IWP_PASSWORD || 'test';
+
+const extractJSONString = (sourceLines, variableName, fileName) => {
+  const regexp = new RegExp(`^var ${variableName} = extractJSON\\('`);
+  const listingLine = sourceLines.filter(line => line.search(regexp) !== -1)[0];
+  if (listingLine.length === 0) {
+    console.error(`${variableName} could not be found`);
+    process.exit(1);
+  }
+  const listingJson = listingLine
+    .replace(regexp, '')
+    .replace(/'\);$/, '');
+
+  const listing = JSON.parse(listingJson);
+  writeFile(path.join(__dirname, fileName), JSON.stringify(listing, null, 2));
+};
 
 client
   .url('https://members.iracing.com/membersite/login.jsp')
@@ -19,13 +34,12 @@ client
   .addValue('[name="password"]', password)
   .click('input.log-in')
   .url('http://members.iracing.com/membersite/member/Series.do')
-  .waitForVisible('.simpleNav #navigation', 5000)
-  .execute('return TrackListing;')
-  .then(result => writeFile(path.join(__dirname, 'src/data/tracks.json'), JSON.stringify(result.value, null, 2)))
-  .execute('return CarClassListing;')
-  .then(result => writeFile(path.join(__dirname, 'src/data/car-class.json'), JSON.stringify(result.value, null, 2)))
-  .execute('return CarListing;')
-  .then(result => writeFile(path.join(__dirname, 'src/data/cars.json'), JSON.stringify(result.value, null, 2)))
-  .execute('return SeasonListing;')
-  .then(result => writeFile(path.join(__dirname, 'src/data/season.json'), JSON.stringify(result.value, null, 2)))
+  .source()
+  .then(source => {
+    const sourceLines = source.value.split('\n');
+    extractJSONString(sourceLines, 'TrackListing', 'src/data/tracks.json');
+    extractJSONString(sourceLines, 'CarClassListing', 'src/data/car-class.json');
+    extractJSONString(sourceLines, 'CarListing', 'src/data/cars.json');
+    extractJSONString(sourceLines, 'SeasonListing', 'src/data/season.json');
+  })
   .end();
