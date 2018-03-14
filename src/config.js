2ac3cae29bf35d69b740725f36e0dab92b180cb7
Tom Moitié <tomm@magmadigital.co.uk>
2018-03-14 14:19:27 +0000

    New season

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export const seasonStart = moment('2017-12-12T00:00:00+00:00').utc();
-export const seasonEnd = moment('2018-03-14T00:00:00+00:00').utc();
-export const weekSeasonStart = moment('2017-12-12T00:00:00+00:00').utc();
+export const seasonStart = moment('2018-03-13T00:00:00+00:00').utc();
+export const seasonEnd = moment('2018-06-13T00:00:00+00:00').utc();
+export const weekSeasonStart = moment('2018-03-13T00:00:00+00:00').utc();
