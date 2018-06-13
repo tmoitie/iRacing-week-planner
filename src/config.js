6969f50362dcc59339aee12f007366f8f4c986ca
Tom Moitié <tomm@magmadigital.co.uk>
2018-06-13 15:20:10 +0100

    Update to S3 2018

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export const seasonStart = moment('2018-03-13T00:00:00+00:00').utc();
-export const seasonEnd = moment('2018-06-13T00:00:00+00:00').utc();
-export const weekSeasonStart = moment('2018-03-13T00:00:00+00:00').utc();
+export const seasonStart = moment('2018-06-12T00:00:00+00:00').utc();
+export const seasonEnd = moment('2018-09-11T00:00:00+00:00').utc();
+export const weekSeasonStart = moment('2018-06-12T00:00:00+00:00').utc();
