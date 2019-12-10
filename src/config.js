4d4150e1dac936bd803bd51904275cde278079ea
Tom Moitié <tom@tmoitie.co.uk>
2019-12-10 18:26:30 +0000

    Update to new season

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export const seasonStart = moment('2019-09-10T00:00:00+00:00').utc();
-export const seasonEnd = moment('2019-12-09T00:00:00+00:00').utc();
-export const weekSeasonStart = moment('2019-09-10T00:00:00+00:00').utc();
+export const seasonStart = moment('2019-12-10T00:00:00+00:00').utc();
+export const seasonEnd = moment('2020-03-09T00:00:00+00:00').utc();
+export const weekSeasonStart = moment('2019-12-10T00:00:00+00:00').utc();
