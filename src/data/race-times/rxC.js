e4a7d57fa6b9d7ee4799ebdd61140c9ece64ebcb
Tom Moitié <tomm@gendius.co.uk>
2020-09-18 13:03:47 +0100

    Add some race times

--- src/data/race-times/rxC.js
+++ src/data/race-times/rxC.js
@@ -7,4 +7,12 @@ export default [
     everyTime: duration(60, 'minutes'),
     offset: duration(30, 'minutes')
   },
+  {
+    // iRX World Championship Series
+    seriesId: 424,
+    weekEndOffset: duration({ days: 7 }),
+    setTimes: [
+      duration({ days: 4, hours: 17, minutes: 45 }), // Sat 17:45 GMT
+    ]
+  },
 ];
