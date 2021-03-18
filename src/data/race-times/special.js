0dc6a5d58520dd419b5309de067e0e084ba87d06
Tom Moitié <tomm@gendius.co.uk>
2021-03-18 16:55:57 +0000

    Add BMW Simcup

--- src/data/race-times/special.js
+++ src/data/race-times/special.js
@@ -1,15 +1,12 @@
 import { duration } from 'moment';
 
 export default [
-  // {
-  //   // Bathurst 12
-  //   seriesId: 329,
-  //   setTimes: [
-  //     duration({ days: 3, hours: 22 }),
-  //     duration({ days: 4, hours: 7 }),
-  //     duration({ days: 4, hours: 12 }),
-  //     duration({ days: 4, hours: 16 }),
-  //   ],
-  //   weekEndOffset: duration({ days: 4 }),
-  // },
+  {
+    // BMW Sim GT Cup
+    seriesId: 437,
+    setTimes: [
+      duration({ days: 5, hours: 13 }),
+    ],
+    weekEndOffset: duration({ days: 4 }),
+  },
 ];
