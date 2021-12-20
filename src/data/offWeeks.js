d44362e30f10ab9b2700e87c6455884f46d6052d
Tom Moitié <tomm@gendius.co.uk>
2021-12-20 17:20:22 +0000

    Fix euro enduro

--- src/data/offWeeks.js
+++ src/data/offWeeks.js
@@ -115,4 +115,11 @@ export default {
       1.1, 2.1, 3.1, 4.1, 5.1,
     ],
   },
+  // Euro Endurance
+  3523: {
+    weekStartOffset: { days: 7 },
+    offWeeks: [
+      1.1, 2.1, 3.1, 4.1, 5.1,
+    ],
+  },
 };
