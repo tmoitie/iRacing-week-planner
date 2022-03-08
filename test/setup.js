cce27114c3bc56fb75c0d41dbf33598f084ce3ed
Tom Moitié <tomm@gendius.co.uk>
2022-03-08 12:30:26 +0000

    Update test files

--- test/setup.js
+++ test/setup.js
@@ -3,5 +3,21 @@ import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom/extend-expect';
 import 'snapshot-diff/extend-expect';
+import '../src/data/season.json';
+import '../src/data/racelengths.json';
+import '../src/data/racetimes.json';
+import '../src/data/cars.json';
+import '../src/data/car-class.json';
+import '../src/data/tracks.json';
+import '../src/data/contributors.json';
+
 
 configure({ adapter: new Adapter() });
+
+jest.mock('../src/data/season.json');
+jest.mock('../src/data/racelengths.json');
+jest.mock('../src/data/racetimes.json');
+jest.mock('../src/data/cars.json');
+jest.mock('../src/data/car-class.json');
+jest.mock('../src/data/tracks.json');
+jest.mock('../src/data/contributors.json');
