9e03973dd930807b2fee04765f6e12932140377e
Tom Moitié <tomm@gendius.co.uk>
2022-03-15 08:58:35 +0000

    Update changelog test

---
+++ src/data/__mocks__/changelog.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+
+export default [{
+  date: moment('2015-12-16 00:00:00').utc(),
+  items: [
+    'Add ability to \'favorite\' content with the star icon.',
+    'Fix bug with disabled default content.',
+  ],
+}, {
+  date: moment('2015-12-15 00:00:00').utc(),
+  items: [
+    'Add "Select All Oval/Road" to content choosers.',
+    'Made free content un-unselectable.',
+    'Made series clickable with popup showing all race weeks.',
+    'Add licence class column to table.',
+  ],
+}];
