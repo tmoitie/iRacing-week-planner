70cf5d6b4722554471fa576db1941f0d03e65db4
coldsc <blackdragonil@hotmail.com>
2022-05-06 00:31:29 -0700

    RU translation file

---
+++ translations/ru.js
@@ -0,0 +1,121 @@
+export default {
+    translation: {
+      // СЛАВА УКРАИНЕ ГЕРОЯМ СЛАВА
+      'iRacing Week Planner': 'Еженедельный Планировщик iRacing',
+      'Purchase guide': 'Гид Покупок',
+      'Set my tracks': 'Трассы',
+      'Set my cars': 'Автомобили',
+      'Set favorite series': 'Любимые Серии',
+      'Options': 'Опции',
+      'About': 'О Приложение',
+      'Week {{week}}': 'Неделя {{week}}',
+      'Races for date: {{date, YYYY MMM DD}}': 'День Гонок: {{date, DD MM YYYY}}',
+      'No time data': 'Нету',
+  
+      // Filters
+      'Filters': 'Фильтр',
+      'Type': 'Тип',
+      'Oval': 'Овал',
+      'Dirt': 'Грязь',
+      'Road': 'Дорога',
+      'RX': 'Ралли Кросс', // Rally Cross/Road Dirt
+      'Licence': 'Лицензия',
+      'Official/Fixed': 'Официальная/Фиксированная',
+      'Unofficial': 'Неофициальная',
+      'Official': 'Официальная',
+      'Open setup': 'Открытые Настройки',
+      'Fixed setup': 'Фиксированные Настройки',
+      'Content': 'Контент',
+      'Owned cars only': 'Только собственные авто',
+      'Owned tracks only': 'Только собственные трассы',
+      'Favorite series only': 'Только любимые серии',
+      'Favorite cars only': 'Только любимые авто',
+      'Favorite tracks only': 'Только любимые трассы',
+      'Reset filters': 'Сбросить фильтры',
+      'Reset all settings': 'Сбросить все настройки',
+  
+      // Column Headers
+      'Columns': 'Столбцы',
+      'ID': 'ID',
+      'Class': 'Тип',
+      'Series': 'Серии',
+      'Track': 'Трасса',
+      'Car': 'Автомобиль',
+      'Start': 'Старт',
+      'End': 'Конец',
+      'Fixed': 'Фиксированный',
+      'Race times': 'Время гонок',
+      'Next race': 'Следующая гонка',
+      'Season end': 'Конец сезона',
+      'Link': 'Ссылка',
+      'Week': 'Неделя',
+      'Count': 'Счет',
+      'Length': 'Длина',
+  
+      // Race length
+      'No data': 'Нету',
+      '{{laps}}L': '{{laps}} круг',
+      '{{minutes}}m': '{{minutes}} мин',
+      '{{hours}}h': '{{hours}} час',
+  
+  
+      // Modals
+      'Use the checkbox to set the content you own, and use the star to set your favourite content.':
+        'Используйте флажок, чтобы выбрать свой контент, и используйте звездочку, чтобы выбрать свой любимый контент.',
+      'Select all': 'Выбрать все',
+      'Select all oval': 'Выбрать все овальные',
+      'Select all road': 'Выбрать все дорожные',
+      'Choose favorite series': 'Выбрать любимые серии',
+      'Close': 'Закрыть',
+      'Contributors': 'Авторы',
+      'Changelog': 'Изменении',
+      'Purchase Guide': 'Гид Покупок',
+      'This tool was created by <2>tmoitie</2> (<5>Tom Moitié</5> on iRacing). Feel free to contact me via twitter or iRacing if you have any feedback or questions. The code is hosted publicly on <9>Github</9>. Thanks!':
+        '<2>tmoitie</2> создал это приложение (<5>Tom Moitié</5> на iRacing). Не стесняйтесь обращаться со мной Twitter или iRacing. Код в открытом формате на <9>Github</9>. Спасибо!',
+      'Loading': 'Загрузка',
+      'Cars for {{series}}': 'Авто для {{series}}',
+      'Tracks for {{series}}': 'Трассы для {{series}}',
+      'These unowned tracks from your favorite series appear multiple times for this season. You can purchase these tracks with the direct link.': 
+      'Эти трассы из ваших любимых серии появляются несколько раз в этом сезоне. Вы можете купить эти трассы по прямой ссылке',
+  
+      // Dates
+      '{{date, YYYY MMMM DD}}': '{{date, YYYY MMMM DD}}',
+      '{{date, YYYY-MM-DD}}': '{{date, YYYY-MM-DD}}',
+      'Every {{every}} starting at {{time, H:mm}} UTC': 'Каждый {{every}} начиная {{time, HH:mm z}}',
+      '{{timeLocal, ddd h:mma}} ({{timeUtc, ddd h:mma z}})': '{{timeLocal, ddd HH:mm}} ({{timeUtc, ddd HH:mm z}})',
+      'Set times': 'Выбор времен',
+      '{{date, ddd k:mm}}': '{{date, ddd k:mm}}',
+  
+      'This is now costing me a few quid a month to run, so if you like using this tool, please feel free to <2>buy me a coffee</2>.':
+        'Для меня это не бесплатно, если нравиться, <2>купите мне чашку кофе</2>.',
+      'Buy me a coffee': 'Купить мне чашку кофе',
+      'Synced': 'Синх',
+      'Awaiting sync': 'Ждем синх',
+      '(refresh browser to download latest)': '(перезагрузи браузер скачать новое)',
+      'Reset Password': 'Забыл Пароль?',
+      'Enter your email address to reset your password.': 'Введите свой адрес почты для восстановления пароля.',
+      'Email address': 'Почта',
+      'Submit': 'Вход',
+      'Thanks, please check your email for further details.': 
+       'Спасибо, обращайтесь почте для детали',
+      'Sign in/Create account': 'Зарегистрироваться ',
+      'Signing in will sync your settings across multiple browsers or devices.': 
+       'Вход даст вам синх на своих устройствах.',
+      'When you first sign in it will use your current settings to set up the account, but once an account is set-up logging in will overwrite any settings you stored as a guest.':
+        'Когда сделаешь логин будет использованы текущие настройки. Вход тогда будет перерисовать настройки гостя',
+      'Password': 'Пароль',
+      'Forgotten password?': 'Забыл пароль?',
+      'Sign in': 'Вход',
+      'Sign out': 'Выход',
+      'Create account': 'Зарегистрироваться',
+      'There is no user record corresponding to this identifier. The user may have been deleted.': 
+       'Нет записи пользователя, возможно удален.',
+      'The password is invalid or the user does not have a password.': 
+       'Неправильный Пароль.',
+      'The email address is badly formatted.': 'Формат пароля не подходит.',
+      'The password must be 6 characters long or more.': 'Пароль должен состоять из 6 символов или более.',
+      'The email address is already in use by another account.': 'Почта уже исползана.',
+      'Password should be at least 6 characters': 'Пароль должен состоять из 6 символов или более.',
+    },
+  };
+  
