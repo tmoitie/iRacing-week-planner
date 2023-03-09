e3886b6cbfb83c7cac57e3aebd4dca08f5a1b22e
Tom Moitié <tomm@gendius.co.uk>
2023-03-09 15:36:54 +0000

    Remove old season adjustments

--- src/data/offWeeks.js
+++ src/data/offWeeks.js
@@ -1,54 +1,17 @@
 export default {
-  // 437 | 3591 | BMW Sim GT Cup
-  3591: {
-    offWeeks: [
-      1.1, 1.1, 1.1, 2.1, 2.1, 2.1, 2.1, 2.1, 2.1, 3.1, 3.1, 3.1, 4.1, 4.1, 4.1, 4.1,
-      4.1, 5.1, 5.1, 6.1, 7.1, 7.1, 7.1, 7.1, 8.1, 8.1, 8.1, 8.1, 9.1, 9.1, 9.1,
-    ],
-  },
-  // 207 | 3589 │ NASCAR iRacing Series - Fixed
-  3589: {
-    offWeeks: [
-      13.1, 16.1,
-    ],
-  },
-  // 229 | 3590 │ NASCAR iRacing Series - Open
-  3590: {
-    offWeeks: [
-      13.1, 16.1,
-    ],
-  },
+  // Commented out series kept in here for when iRacing schedule next season, often with the same settings
   // 374 │ 3598 │ IndyCar iRacing Series
-  3598: {
-    offWeeks: [
-      1.1, 1.1, 2.1, 2.1, 3.1, 3.1, 4.1, 5.1, 8.1, 8.1, 9.1, 13.1, 14.1,
-    ],
-  },
-  // 495 │ 3691 │ iRacing Grand Prix Tour
-  3691: {
-    offWeeks: [
-      2.1, 3.1, 4.1, 5.1, 7.1, 9.1, 11.1, 13.1, 13.2, 13.3, 16.1, 19.1, 21.1,
-    ],
-  },
-  // 496 │ 3692 | iRacing Grand Prix Tour - Fixed
-  3692: {
-    offWeeks: [
-      2.1, 3.1, 4.1, 5.1, 7.1, 9.1, 11.1, 13.1, 13.2, 13.3, 16.1, 19.1, 21.1,
-    ],
-  },
-  // 451 │ 3715 │ Creventic Endurance Series
-  3715: {
-    offWeeks: [
-      1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 2.1, 2.1, 2.1, 2.1, 2.1, 2.1, 2.1, 2.1, 3.1, 3.1, 3.1,
-      3.1, 3.1, 3.1, 3.1, 3.1, 3.1, 3.1,
-    ],
-  },
+  // 3598: {
+  //   offWeeks: [
+  //     1.1, 1.1, 2.1, 2.1, 3.1, 3.1, 4.1, 5.1, 8.1, 8.1, 9.1, 13.1, 14.1,
+  //   ],
+  // },
   // 275 │ 3720 │ Nurburgring Endurance Championship
-  3720: {
-    offWeeks: [
-      1.1, 2.1, 2.1, 3.1, 3.1, 3.1, 3.1, 4.1, 5.1, 5.1, 6.1, 6.1, 6.1, 7.1, 7.1, 8.1, 8.1,
-    ],
-  },
+  // 3720: {
+  //   offWeeks: [
+  //     1.1, 2.1, 2.1, 3.1, 3.1, 3.1, 3.1, 4.1, 5.1, 5.1, 6.1, 6.1, 6.1, 7.1, 7.1, 8.1, 8.1,
+  //   ],
+  // },
   // 469 │ 3779 | Global Endurance Pure Driving School Series
   // 3779: {
   //   weekStartOffset: { days: 7 },
@@ -57,70 +20,70 @@ export default {
   //   ],
   // },
   // 439 │ 3816 │ eNASCAR Road to Pro Qual Series - Round 2
-  3816: {
-    offWeeks: [
-      1.1, 2.1, 3.1, 4.1, 5.1, 6.1,
-    ],
-  },
+  // 3816: {
+  //   offWeeks: [
+  //     1.1, 2.1, 3.1, 4.1, 5.1, 6.1,
+  //   ],
+  // },
   // 512 │ 3974 | SCCA Runoffs - FR500
-  3974: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3974: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 506 │ 3968 | SCCA Runoffs - Formula Vee
-  3968: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3968: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 509 │ 3971 | SCCA Runoffs - GT1
-  3971: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3971: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 511 │ 3973 | SCCA Runoffs - GT4
-  3973: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3973: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 508 │ 3970 | SCCA Runoffs - Global MX-5 Spec Miata
-  3970: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3970: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 507 │ 3969 | SCCA Runoffs - Spec Racer Ford
-  3969: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3969: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 513 │ 3975 | SCCA Runoffs - Touring Cars
-  3975: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3975: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 510 │ 3972 | SCCA Runoffs - USF2000
-  3972: {
-    weekEndOffset: { days: 7 },
-  },
+  // 3972: {
+  //   weekEndOffset: { days: 7 },
+  // },
   // 478 │ 3965 │ eNASCAR RTP Contender Series
-  3965: {
-    weekStartOffset: { days: 6 },
-    offWeeks: [
-      3.1,
-    ],
-  },
+  // 3965: {
+  //   weekStartOffset: { days: 6 },
+  //   offWeeks: [
+  //     3.1,
+  //   ],
+  // },
   // 492 │ 4007 │ IMSA Michelin Pilot Challenge Series
-  4007: {
-    weekStartOffset: { days: 7 },
-    offWeeks: [
-      1.1, 2.1, 3.1, 4.1, 5.1,
-    ],
-  },
+  // 4007: {
+  //   weekStartOffset: { days: 7 },
+  //   offWeeks: [
+  //     1.1, 2.1, 3.1, 4.1, 5.1,
+  //   ],
+  // },
   // 419 │ 4037 │ IMSA Endurance Series
-  4037: {
-    offWeeks: [
-      1.1, 2.1, 3.1, 4.1, 5.1,
-    ],
-  },
+  // 4037: {
+  //   offWeeks: [
+  //     1.1, 2.1, 3.1, 4.1, 5.1,
+  //   ],
+  // },
   // 331 │ 4036 │ European Endurance Pure Driving School Series
-  4036: {
-    weekStartOffset: { days: 7 },
-    offWeeks: [
-      1.1, 2.1, 3.1, 4.1, 5.1,
-    ],
-  },
+  // 4036: {
+  //   weekStartOffset: { days: 7 },
+  //   offWeeks: [
+  //     1.1, 2.1, 3.1, 4.1, 5.1,
+  //   ],
+  // },
   // 207 │ 4114 │ NASCAR iRacing Series - Fixed
   4114: {
     offWeeks: [
